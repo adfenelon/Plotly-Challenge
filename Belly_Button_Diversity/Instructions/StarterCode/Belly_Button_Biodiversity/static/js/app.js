20ce08be70f255fae20d14d97c290bf9680af130
Alexandra Fenelon <alexandra.fenelon1122@gmail.com>
2019-09-30 11:01:35 -0400

    adding plotly updates

---
+++ Belly_Button_Diversity/Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -0,0 +1,52 @@
+function BuildBellyButtonMetaData(sample) {
+  d3.json(`/metadata/${sample}`).then((data) => {
+      var PANEL = d3.select("#sample-metadata");
+      PANEL.html("");
+
+      Object.entries(data).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key}: ${value}`);
+      });
+
+      buildGuage(data.WFREQ);
+  });
+}
+
+function buildCharts(sample) {
+
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+
+    // @TODO: Build a Bubble Chart using the sample data
+
+    // @TODO: Build a Pie Chart
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // otu_ids, and labels (10 each).
+}
+
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("/names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+}
+
+// Initialize the dashboard
+init();
